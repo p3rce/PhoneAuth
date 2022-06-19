01a3c4ffa29a53864ee589745b9a8c816dc99629
Pierce Goulimis <piercemg2013@gmail.com>
2022-06-18 23:04:10 -0400

    Initial Commit

---
+++ PhoneAuth/PhoneAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneAuthApp.swift
+//  PhoneAuth
+//
+//  Created by Pierce Goulimis on 2022-06-18.
+//
+
+import SwiftUI
+
+@main
+struct PhoneAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
